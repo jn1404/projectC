9a9431c36f16299d059f6bc963ed5dd51e6000ef
hcm284 <hcm284@nyu.edu>
2016-04-22 18:54:43 -0400

    Update game.c

--- game.c
+++ game.c
@@ -7,6 +7,9 @@ int main()
 { 
 	char user;
 	int cmp;
+	int cmp2;
+	int cmp3;
+	int dagger;
 
 	while(1) // Infinite loop
 	{
@@ -19,44 +22,64 @@ int main()
 		else
 			printf("\n\nI don't know the word %s", user);
 		}
-		
+		fgets(user, sizeof user, stdin);
+		cmp2 = strcmp(user, "go north");
+		if (cmp2 == 0){						// if player goes north
+			printf("\nYou are in front of the house. The door seems to be unlocked.")
+			fgets(user, sizeof user, stdin);
+			cmp3 = strcmp(user, "open door");
+			if (cmp3 == 0){
+				printf("\n The interior is quaint and dark. There is a small kitchen to your right, a 
+		basement to your left and stairs in front of you.");
+				if(){					// if player goes north
+
+				}
+				else if(){
+
+				}
+				else if(){
+
+				}
+				else(){
+
+				}
+			}
+			else if(){ 		// if player tries to go back south
+
+			}
+			else(){		// if player does anything else
+
+			}
+		}
+		else if(){			// if player goes south
+
+		}
 	}
 
 
 	/* STORY: LIKE ZORK 
-
 	Intro:
 	You are standing in a mossy field and see an old house in the distance. 
 	There is a letter here. 
-
 	>read letter
 	"Welcome to 'title of game'"
-
 	Depending on what user inputs: 
 	>go north
 	Front of House
 	You are in front of the house. The door seems to be open. 
-
 		>enter door 
 		Floor 1 
 		The interior is quaint and dark. There is a small kitchen to your right, a 
 		basement to your left and stairs in front of you.
-
 		>go left 
 		You walk down the stairs into a dark basement. There is something in the corner.
-
 			>check corner
 			You have found a short dagger. It is decorated with many engravings.
-
 			>go upstairs
 			You are back in the first floor. 
-
 		>go right
 		You walk into the kitchen. There is some food cooking in a large pot.
-
 		>
-
-
 	>go south 
 	The Woods
 	There seems to be a bridge in the distance. 
@@ -69,16 +92,12 @@ int main()
 			
 				>fight 
 				The goblin knocks you out and takes you to its cave. 
-
 					>
 				
 				>run
 				You run towards the house. The goblin stops chasing you and returns 
 				to the bridge. 
 			
-
 	
 	
-
-
 }
